1fac7941c1c265d5f42c73d053d8275c8ee9f9eb
gefest <gefest@me.com>
2020-11-26 20:08:30 +0300

    les 1 HW

--- parse5ka.py
+++ parse5ka.py
@@ -42,16 +42,44 @@ class Parse5ka:
     def run(self):
         for products in self.parse(self.start_url):
             for product in products:
-                self._save_to_file(product)
+                self._save_to_file(product, product['id'])
             time.sleep(0.1)
 
     @staticmethod
-    def _save_to_file(product):
-        path = Path(os.path.dirname(__file__)).joinpath('products').joinpath(f'{product["id"]}.json')
+    def _save_to_file(product, file_name):
+        path = Path(os.path.dirname(__file__)).joinpath('products').joinpath(f'{file_name}.json')
         with open(path, 'w', encoding='UTF-8') as file:
             json.dump(product, file, ensure_ascii=False)
 
 
+class ParserCatalog(Parse5ka):
+
+    def __init__(self, start_url, category_url):
+        self.category_url = category_url
+        super().__init__(start_url)
+
+    def get_categories(self, url):
+        response = self._get(url, headers=self._headers)
+        return response.json()
+
+    def run(self):
+        for category in self.get_categories(self.category_url):
+            data = {
+                "name": category['parent_group_name'],
+                'code': category['parent_group_code'],
+                "products": [],
+            }
+
+            self._params['categories'] = category['parent_group_code']
+
+            for products in self.parse(self.start_url):
+                data["products"].extend(products)
+            self._save_to_file(
+                data,
+                category['parent_group_code']
+            )
+
+
 if __name__ == '__main__':
-    parser = Parse5ka('https://5ka.ru/api/v2/special_offers/')
+    parser = ParserCatalog('https://5ka.ru/api/v2/special_offers/', 'https://5ka.ru/api/v2/categories/')
     parser.run()
