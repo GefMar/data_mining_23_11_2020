d582f26e19dcd9bd3fd2d3f771bcfeefe7595a98
gefest <gefest@me.com>
2020-12-17 19:55:04 +0300

    les 7

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from scrapy.settings import Settings
 
 from gb_parse.spiders.autoyoula import AutoyoulaSpider
 from gb_parse.spiders.instagram import InstagramSpider
+from gb_parse.spiders.zillow import ZillowSpider
 
 import dotenv
 
@@ -14,5 +15,6 @@ if __name__ == '__main__':
     crawl_settings.setmodule('gb_parse.settings')
     # crawl_settings.setmodule(settings)
     crawl_proc = CrawlerProcess(settings=crawl_settings)
-    crawl_proc.crawl(InstagramSpider, login=os.getenv('LOGIN'), password=os.getenv('PASSWORD'), tag_list=['python', ])
+    # crawl_proc.crawl(InstagramSpider, login=os.getenv('LOGIN'), password=os.getenv('PASSWORD'), tag_list=['python', ])
+    crawl_proc.crawl(ZillowSpider)
     crawl_proc.start()
